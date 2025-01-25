565e3ac3a5baa6023682b49bc0977d31866ab255
Shristi <shristimahato6054@gmail.com>
2025-01-25 19:34:45 +0545

    added background image and discount section in Home page

--- src/components/JSFile/Home.js
+++ src/components/JSFile/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import bimage from '../images/background-home.jpg';
+import '../CSSFile/home.css'
 
 function Home() {
-    return<h1>Home</h1> ;
+    return(
+        <div className="home">
+            <div className="discount">
+                <p>Get 20% discount on every <span>FRIDAY SALE</span></p>
+            </div>
+            <div className="banner">
+                <img src={bimage} className="background-image" alt="background-image"/>
+            </div>
+        </div>
+    ) ;
 }
 
 export default Home;
